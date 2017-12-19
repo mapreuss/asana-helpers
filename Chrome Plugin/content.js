5f9f35e2bae6dc55cfed45846f9cf7e1d89071eb
Marta Preuss <marta.preuss@chaordicsystems.com>
2017-12-19 15:37:18 -0200

    Chrome plugin documentation

--- Chrome Plugin/content.js
+++ Chrome Plugin/content.js
@@ -4,37 +4,39 @@ chrome.runtime.onMessage.addListener(
     if( request.message === "copy_task" ) {
 
             var uncheck = function(element){
-                $(element).removeClass("checkboxWidget--checked");
-                $(element).addClass("checkboxWidget--unchecked");
+                element = document.getElementById(element);
+                element.classList.remove('checkboxWidget--checked');
+                element.classList.add("checkboxWidget--unchecked");
             };
 
             var check = function(element){
-                $(element).addClass("checkboxWidget--checked");
-                $(element).removeClass("checkboxWidget--unchecked");
+                element = document.getElementById(element);
+                element.classList.add('checkboxWidget--checked');
+                element.classList.remove("checkboxWidget--unchecked");
             };
+
 
             // Open Copy Task screen
             $(".SingleTaskPaneExtraActionsButton")[0].click();
-            $(".SingleTaskPaneExtraActionsButton-makeACopy")[0].click(function(){
+            $(".SingleTaskPaneExtraActionsButton-makeACopy")[0].click();
+
+            // Wait until popup is opened
+            setTimeout(function(){ 
                 // Uncheck
-                uncheck("#duplicate_object_option_task_description");
-                uncheck("#duplicate_object_option_assignee");
-                uncheck("#duplicate_object_option_subtasks");
-                uncheck("#duplicate_object_option_attachments");
-                uncheck("#duplicate_object_option_due_date");
-                uncheck("#duplicate_object_option_dependencies");
+                uncheck("duplicate_object_option_task_description");
+                uncheck("duplicate_object_option_assignee");
+                uncheck("duplicate_object_option_subtasks");
+                uncheck("duplicate_object_option_attachments");
+                uncheck("duplicate_object_option_due_date");
+                uncheck("duplicate_object_option_dependencies");
 
                 // Check
-                check("#duplicate_object_option_tags");
-                check("#duplicate_object_option_followers");
-                check("#duplicate_object_option_projects");
-                check("#duplicate_object_option_parent_task");
-            });
+                check("duplicate_object_option_tags");
+                check("duplicate_object_option_followers");
+                check("duplicate_object_option_projects");
+                check("duplicate_object_option_parent_task");
 
-
-            // $("#duplicate_object_dialog_submit").click();
-
-            console.log("triggered");
+            }, 1500);
 
     }
   }
--- README.md
+++ README.md
@@ -2,4 +2,38 @@
 
 Some shortcuts to asana.
 
-Please asana, don't sue me.
+Please asana, don't sue me. 
+
+---
+
+## Chrome plugin
+
+### Installation
+
+* Download *Chrome plugin* folder;
+* Access [chrome://extensions](chrome://extensions) and select `Developer mode`.
+* Select `Load unpacked extension...` and select *Chrome plugin* folder
+
+Optional:
+
+* Scroll Chrome Extensions page untill the end and click on `Keyboard Shortcuts`
+* Add a shortcut to this extension.
+
+### Usage
+
+When in a task, use the shortcut or click Chrome button. It'll show the "copy task" pop-up with some options unsellected. Click "Create task" and edit it later.
+
+
+---
+
+## To do
+
+* Selected options remain (dis)selected when click in task name
+* Change task name removing "Copy task" and changing [DSN] to [QA]
+* Plugin options page, to make last item optional.
+
+## Changelog
+
+*December 19, 2017*
+We now have a Chrome plugin which duplicates tasks. It's still a work in progress.
+
